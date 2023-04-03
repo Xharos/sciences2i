1c0a3eec3469ff39b5d38060fb1aed63c4f497e3
Xharos <devxharos@gmail.com>
2023-04-03 19:18:13 +0200

    Update template exemple to include the new svelte components

--- .github/template.svelte
+++ .github/template.svelte
@@ -1,5 +1,9 @@
 <script>
     import Scroller from "$lib/components/Scroller.svelte";
+    import Message from "$lib/components/MessageBox.svelte";
+    import HideMessage from "$lib/components/HideMessageBox.svelte";
+    import Framed from "$lib/components/Framed.svelte";
+    import Figure from "$lib/components/Figure.svelte";
     import {Crumbs} from '$lib/components/Crumbs.svelte';
     import K from "$lib/components/K.svelte";
 
@@ -34,5 +38,40 @@
     <div slot="foreground">
         <h1>Mon premier titre</h1>
 
+        <p>Bloc coloré avec compteur intégré :</p>
+
+        <!-- type peut être property, definition, remember -->
+        <Message title="Masse volumique" type="property">
+            La masse volumique (noté
+            <K math={"\\rho"}/>
+            ) est une propriété caractéristique qui représente la quantité de matière (masse) se trouvant dans un espace
+            (une unité de volume) donné.
+        </Message>
+
+        <p>Bloc caché qui apparaît lors du clic de l'utilisateur :</p>
+        <!-- un bloc ou l'utilisateur doit cliquer pour l'étendre en longueur -->
+        <HideMessage title="Démonstration">
+            La démonstration ici est normalement caché..
+        </HideMessage>
+
+        <p>Bloc tout simple qui ne sert exclusivement que pour les remarques</p>
+        <Framed>
+            Ceci est une remarque !
+        </Framed>
+
+        <p>
+            Le mode math peut s'intégrer en ligne (comme ici
+            <K math={"10^{-4}\\space m.s^{-1}"}/>
+            ) ou bien sur plusieurs :
+            <K displayMode math={"U_{AB} = V_A - V_B"}/>
+        </p>
+
+        <h2>Ajouter des images</h2>
+        <p>L'image est ajouté dans un container centré et aux bonnes dimensions. Il est possible de le re-dimensionner
+            en utilisant les classes adaptés avec Bulma (image is-3by-1 par exemple. Privilégier des noms évidents
+            d'image, le format webp et une mise en page type bannière pour ne pas occuper tout l'espace utile..</p>
+        <Figure legend="coucou toi">
+            <img src="$lib/assets/elec.webp">
+        </Figure>
     </div>
 </Scroller>
