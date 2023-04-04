425c26436b17c5a3c2f98a424c8790e3be6cd3c0
Xharos <devxharos@gmail.com>
2023-04-04 10:12:17 +0200

    Fix footer alignment #11

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,56 +49,50 @@
         </div>
     </div>
 </nav>
-<section class="hero">
-    <div class="hero-body" style="padding: 2rem">
-        <slot/>
-    </div>
-</section>
 
-<footer class="footer">
+<main>
+    <section class="hero">
+        <div class="hero-body" style="padding: 2rem">
+            <slot/>
+        </div>
+    </section>
+</main>
+
+<footer class="section">
     <div class="container">
-        <div class="columns">
-            <div class="column is-1 has-text-centered">
-                <a
-                        alt="Poursuivre vers la chaîne Youtube de Valentin Burgaud"
-                        class="icon-text"
-                        href="https://www.youtube.com/channel/UC197VifUbGS9iqCiYUe0hSA"
-                >
+        <div class="pb-5 is-flex is-flex-wrap-wrap is-justify-content-between is-align-items-center">
+            <div class="mr-auto mb-2">
+                <a class="is-inline-block" href="/">
+                    <img alt="Logo du site, casque de chantier sur un engrenage" class="image"
+                         src="$lib/assets/favicon_32x32.webp" width="32px">
+                </a>
+            </div>
+            <div>
+                <ul class="is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-center">
+                    <li class="mr-4"><a class="button is-white" href="/lessons">Cours</a></li>
+                    <li class="mr-4"><a class="button is-white" href="/cards">Fiches</a></li>
+                    <li class="mr-4"><a class="button is-white" href="/tutorials">Tutoriels</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="pt-5" style="border-top: 1px solid #dee2e6;"></div>
+    <div class="container">
+        <div class="is-flex-tablet is-justify-content-between is-align-items-center">
+            <p><span class="tag is-primary">2023-{date}</span> | Valentin BURGAUD</p>
+            <div class="py-2 is-hidden-tablet"></div>
+            <div class="ml-auto">
+                <a class="mr-4 is-inline-block"
+                   href="https://www.youtube.com/channel/UC197VifUbGS9iqCiYUe0hSA">
                     <span class="icon">
                         <Fa icon={faYoutube} primaryColor="#FF0000"/>
                     </span>
-                    <span>Youtube</span>
                 </a>
-            </div>
-            <div class="column is-1 has-text-centered">
-                <a
-                        alt="Accéder au stockage sur github de ce site"
-                        class="icon-text"
-                        href="https://github.com/Xharos/sciences2i"
-                >
+                <a class="mr-4 is-inline-block" href="https://github.com/Xharos/sciences2i">
                     <span class="icon">
                         <Fa icon={faGithub} primaryColor="#171515"/>
                     </span>
-                    <span>Github</span>
                 </a>
-            </div>
-            <div class="column is-1 has-text-centered">
-                <a
-                        href="http://creativecommons.org/licenses/by-nc/4.0/"
-                        rel="license"
-                >
-                    <img
-                            alt="Licence Creative Commons"
-                            height="31"
-                            src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
-                            style="border-width:0"
-                            width="88"
-                    />
-                </a>
-            </div>
-            <div class="column is-9 has-text-centered">
-                <small class="text-light"/>
-                <span class="tag is-primary">2023-{date}</span> | Valentin BURGAUD
             </div>
         </div>
     </div>
