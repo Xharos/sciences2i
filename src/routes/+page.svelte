a226606b65998791ca316a450bae5f41028008ef
Xharos <devxharos@gmail.com>
2023-04-04 11:37:40 +0200

    Fix #13

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,164 +42,166 @@
 <!-- svelte-ignore a11y-img-redundant-alt -->
 <div class="columns is-centered">
     <div class="column is-11">
-        <div class="content">
-            <div class="wrapper-test">
-                <div class="icon-text is-size-3 has-text-primary">
+        <section class="section">
+            <div class="content">
+                <div class="wrapper-test">
+                    <div class="icon-text is-size-3 has-text-primary">
                     <span class="icon">
                         <Fa icon={faScrewdriverWrench}/>
                     </span>
-                    <span class="space">S2I</span>
+                        <span class="space">S2I</span>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="columns is-desktop">
-            <div class="column is-12-tablet is-7-desktop">
-                <div class="content">
-                    <p>
-                        Ce site recense mes années d'expériences en enseignement
-                        des sciences de l'ingénieur. Il est destiné tout
-                        particulièrement aux élèves et aux professeurs des
-                        classes de l'enseignement supérieur.
-                    </p>
-                    <p>
-                        Tu pourras y retrouver un ensemble de cours
-                        interractifs, tutoriels et de fiches sur les grands
-                        thèmes de la mécatronique.
-                    </p>
-                    <p>
-                        Cette page recense de plus les dernières vidéos publiées
-                        sur ma chaîne Youtube, directement en lien avec les
-                        sciences de l'ingénieur ou bien le monde de l'éducation.
-                    </p>
+            <div class="columns is-desktop">
+                <div class="column is-12-tablet is-7-desktop">
+                    <div class="content">
+                        <p>
+                            Ce site recense mes années d'expériences en enseignement
+                            des sciences de l'ingénieur. Il est destiné tout
+                            particulièrement aux élèves et aux professeurs des
+                            classes de l'enseignement supérieur.
+                        </p>
+                        <p>
+                            Tu pourras y retrouver un ensemble de cours
+                            interractifs, tutoriels et de fiches sur les grands
+                            thèmes de la mécatronique.
+                        </p>
+                        <p>
+                            Cette page recense de plus les dernières vidéos publiées
+                            sur ma chaîne Youtube, directement en lien avec les
+                            sciences de l'ingénieur ou bien le monde de l'éducation.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="column">
-                <div class="box">
-                    <article class="media">
-                        <figure class="media-left">
-                            <p class="image is-64x64">
-                                <img
-                                        alt="photo de profil"
-                                        class="is-rounded"
-                                        height="256"
-                                        src={valentin.logo}
-                                        width="256"
-                                />
-                            </p>
-                        </figure>
-                        <div class="media-content">
-                            <div class="content">
-                                <p>
-                                    <tag class="has-text-weight-bold">
-                                        {valentin.name}
-                                    </tag>
-                                    <tag
-                                            class="has-text-primary is-size-7 is-italic"
-                                    >
-                                        {valentin.mail}
-                                    </tag>
-                                    <br/>
-                                    {valentin.description}
+                <div class="column">
+                    <div class="box">
+                        <article class="media">
+                            <figure class="media-left">
+                                <p class="image is-64x64">
+                                    <img
+                                            alt="photo de profil"
+                                            class="is-rounded"
+                                            height="256"
+                                            src={valentin.logo}
+                                            width="256"
+                                    />
                                 </p>
-                            </div>
-                            <nav class="level is-mobile">
-                                <div class="level-left">
-                                    <a
-                                            alt="Poursuivre vers la chaîne youtube de Valentin BURGAUD"
-                                            class="level-item"
-                                            href="https://www.youtube.com/channel/UC197VifUbGS9iqCiYUe0hSA"
-                                    >
+                            </figure>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <tag class="has-text-weight-bold">
+                                            {valentin.name}
+                                        </tag>
+                                        <tag
+                                                class="has-text-primary is-size-7 is-italic"
+                                        >
+                                            {valentin.mail}
+                                        </tag>
+                                        <br/>
+                                        {valentin.description}
+                                    </p>
+                                </div>
+                                <nav class="level is-mobile">
+                                    <div class="level-left">
+                                        <a
+                                                alt="Poursuivre vers la chaîne youtube de Valentin BURGAUD"
+                                                class="level-item"
+                                                href="https://www.youtube.com/channel/UC197VifUbGS9iqCiYUe0hSA"
+                                        >
                                         <span class="icon is-medium">
                                             <Fa
                                                     icon={faYoutube}
                                                     primaryColor="#FF0000"
                                             />
                                         </span>
-                                    </a>
-                                    <a
-                                            alt="Accéder au profil Linkedin de Valentin BURGAUD"
-                                            class="level-item"
-                                            href="https://www.linkedin.com/in/valentin-burgaud-72bb451a2/"
-                                    >
+                                        </a>
+                                        <a
+                                                alt="Accéder au profil Linkedin de Valentin BURGAUD"
+                                                class="level-item"
+                                                href="https://www.linkedin.com/in/valentin-burgaud-72bb451a2/"
+                                        >
                                         <span class="icon is-medium">
                                             <Fa
                                                     icon={faLinkedin}
                                                     primaryColor="#0077B5"
                                             />
                                         </span>
-                                    </a>
-                                    <a
-                                            alt="Découvrir le dépôt github de ce projet"
-                                            class="level-item"
-                                            href="https://github.com/Xharos/sciences2i"
-                                    >
+                                        </a>
+                                        <a
+                                                alt="Découvrir le dépôt github de ce projet"
+                                                class="level-item"
+                                                href="https://github.com/Xharos/sciences2i"
+                                        >
                                         <span class="icon is-medium">
                                             <Fa
                                                     icon={faGithub}
                                                     primaryColor="#171515 "
                                             />
                                         </span>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                    </article>
+                                        </a>
+                                    </div>
+                                </nav>
+                            </div>
+                        </article>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="content">
-            <blockquote class="is-primary">
-                Des erreurs peuvent se glisser dans ces pages, merci de m'en
-                faire part par mail ou via le github du projet ou ce site est
-                hébergé.
-            </blockquote>
-        </div>
-        <div class="content">
+            <div class="content">
+                <blockquote class="is-primary">
+                    Des erreurs peuvent se glisser dans ces pages, merci de m'en
+                    faire part par mail ou via le github du projet ou ce site est
+                    hébergé.
+                </blockquote>
+            </div>
+            <div class="content">
             <span class="icon-text is-size-5 has-text-primary">
                 <span class="icon">
                     <Fa icon={faYoutube} primaryColor="#FF0000"/>
                 </span>
                 <span>Dernières vidéos youtube</span>
             </span>
-        </div>
-        <div class="columns">
-            {#if latestIds}
-                {#each latestIds as id}
-                    <div class="column is-4">
-                        <div class="video-responsive">
-                            <YouTube {id}/>
+            </div>
+            <div class="columns">
+                {#if latestIds}
+                    {#each latestIds as id}
+                        <div class="column is-4">
+                            <div class="video-responsive">
+                                <YouTube {id}/>
+                            </div>
                         </div>
-                    </div>
-                {/each}
-            {:else}
-                <p>Loading...</p>
-            {/if}
-        </div>
-        <div class="content">
-            <blockquote class="is-primary">
-                <a
-                        href="http://creativecommons.org/licenses/by-nc/4.0/"
-                        rel="license"
-                >
-                    <img
-                            alt="Licence Creative Commons"
-                            height="31"
-                            src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
-                            style="border-width:0"
-                            width="88"
-                    />
-                </a><br/>
-                L'ensemble des documents présents sur ce site sont mis à disposition
-                selon les termes de la
-                <a
-                        href="http://creativecommons.org/licenses/by-nc/4.0/"
-                        rel="license"
-                >
-                    Licence Creative Commons Attribution - Pas d'Utilisation
-                    Commerciale 4.0 International
-                </a>.
-            </blockquote>
-        </div>
+                    {/each}
+                {:else}
+                    <p>Loading...</p>
+                {/if}
+            </div>
+            <div class="content">
+                <blockquote class="is-primary">
+                    <a
+                            href="http://creativecommons.org/licenses/by-nc/4.0/"
+                            rel="license"
+                    >
+                        <img
+                                alt="Licence Creative Commons"
+                                height="31"
+                                src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
+                                style="border-width:0"
+                                width="88"
+                        />
+                    </a><br/>
+                    L'ensemble des documents présents sur ce site sont mis à disposition
+                    selon les termes de la
+                    <a
+                            href="http://creativecommons.org/licenses/by-nc/4.0/"
+                            rel="license"
+                    >
+                        Licence Creative Commons Attribution - Pas d'Utilisation
+                        Commerciale 4.0 International
+                    </a>.
+                </blockquote>
+            </div>
+        </section>
     </div>
 </div>
